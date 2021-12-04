c6d0979e834991fbbc76d2f13a2fc5616faccbb1
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-12-04 11:55:42 +0200

    ...

---
+++ basics/01 - path.js
@@ -0,0 +1,15 @@
+﻿const path = require('path')
+
+console.log('__dirname:', __dirname)
+console.log('__filename:', __filename)
+
+console.log('')
+
+console.log('Filename:', path.basename(__filename))
+console.log(' Dirname:', path.dirname(__filename))
+console.log(' Extname:', path.extname(__filename))
+console.log('    Join:', path.join('a', 'b', 'c'))
+
+console.log('')
+
+console.log('Parse:', path.parse(__filename))
