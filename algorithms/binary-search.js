8e31a9e1bb8b29dc964164ae9c7920e0f52862c0
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-12-05 22:17:54 +0200

    added algorithm folder

---
+++ algorithms/binary-search.js
@@ -0,0 +1,26 @@
+﻿const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+function search(arr, val) {
+  let start = 0;
+  let end = arr.length;
+  let middle = (start + end) >> 1;
+
+  while(middle >= 0 && middle <= end) {
+
+    if (arr[middle] === val) {
+      return middle;
+    }
+
+    if (val < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+
+    middle = (start + end) >> 1;
+  }
+
+  return -1;
+}
+
+console.log('position index: ', search(arr, 5));
