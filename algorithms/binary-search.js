56a1565cd3f1bd2ee008debd877df6eda4602dbe
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-12-07 20:05:10 +0200

    ...

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -1,21 +1,20 @@
 ﻿const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 function search(arr, val) {
+
   let start = 0;
-  let end = arr.length;
+  let end = arr.length - 1;
   let middle = (start + end) >> 1;
 
-  while(middle >= 0 && middle <= end) {
+  while(start <= end) {
 
-    if (arr[middle] === val) {
+    if (val === arr[middle]) {
       return middle;
     }
 
-    if (val < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
+    val < arr[middle]
+      ? end = middle - 1
+      : start = middle + 1
 
     middle = (start + end) >> 1;
   }
@@ -23,4 +22,6 @@ function search(arr, val) {
   return -1;
 }
 
-console.log('position index: ', search(arr, 5));
+arr.forEach(val => {
+  console.log('position index: ', search(arr, val))
+})
